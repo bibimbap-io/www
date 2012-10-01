523b49f54f50ad92d04af4f24401b78e994043f8
Etienne Kneuss <colder@php.net>
2012-10-01 14:33:44 +0200

    Add proper 404 status for missing pages

--- webroot/index.php
+++ webroot/index.php
@@ -1,8 +1,6 @@
 <?php
     require dirname(__DIR__).'/bootstrap.php';
 
-    require VIEWS_PATH.'/_header.php';
-
     $page = isset($_GET['page']) ? $_GET['page'] : "";
     switch ($page) {
         case 'main':
@@ -25,11 +23,12 @@
             break;
 
         default:
+            header("HTTP/1.0 404 Not Found");
             $menu = "404";
             $view = "404";
             break;
     }
-
+    require VIEWS_PATH.'/_header.php';
     require VIEWS_PATH.'/_menu.php';
     require VIEWS_PATH.'/'.$view.'.php';
     require VIEWS_PATH.'/_footer.php';
